09f2c80da79922dac94f314bbe2c66a8f565d0e3
Taulant Ibraj <tibraj94@gmail.com>
2020-04-20 15:50:29 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,20 +1,20 @@
 class BankAccount
   attr_accessor :balance, :status
-  attr_reader :name 
+  attr_reader :name
 
   def initialize(name, balance=1000, status="open")
-    @name = name 
-    @balance = balance 
-    @status = status 
-  end 
+    @name = name
+    @balance = balance
+    @status = status
+  end
 
   def deposit(amount)
     self.balance += amount
-  end 
+  end
 
   def withdrawal(amount)
     self.balance -= amount
-  end 
+  end
 
   def display_balance
   "Your balance is #{self.balance}"
@@ -23,12 +23,12 @@ class BankAccount
   def valid?
     if self.status == "open" && self.balance > 0
       true
-    else 
-      false 
-    end 
-  end 
+    else
+      false
+    end
+  end
 
   def close_account
     self.status = "closed"
-  end  
+  end
 end
